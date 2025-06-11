a65a027fd104657fa853ada22610e217a394a1bd
Nithit Lertcharoensombat <xXNeonKitsuneXx@gmail.com>
2025-06-11 14:20:56 +0700

    add push backend reuse structure

---
+++ server/internal/service/upload_service.go
@@ -0,0 +1,42 @@
+package service
+
+import (
+	"context"
+	"fmt"
+	"github.com/spf13/viper"
+	"mime/multipart"
+
+	"github.com/minio/minio-go/v7"
+)
+
+type uploadService struct {
+	client *minio.Client
+}
+
+func NewUploadService(client *minio.Client) UploadService {
+	return &uploadService{client: client}
+}
+
+func (s *uploadService) UploadFile(file *multipart.FileHeader) (*string, error) {
+	ctx := context.Background()
+	buffer, err := file.Open()
+	if err != nil {
+		return nil, err
+	}
+
+	defer buffer.Close()
+
+	fileName := file.Filename
+
+	bucketName := viper.GetString("minio.bucket")
+
+	_, err = s.client.PutObject(ctx, bucketName, fileName, buffer, file.Size, minio.PutObjectOptions{
+		ContentType: file.Header.Get("Content-Type"),
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	fileURL := fmt.Sprintf("https://minio.bocchikitsunei.com/fairnest/%s", fileName)
+	return &fileURL, err
+}
---
+++ server/main.go
@@ -0,0 +1,129 @@
+package main
+
+import (
+	"fmt"
+	jwtware "github.com/gofiber/contrib/jwt"
+	"github.com/gofiber/fiber/v2"
+	"github.com/minio/minio-go/v7"
+	"gorm.io/driver/postgres"
+
+	"fairnest/internal/entities"
+	"fairnest/internal/handler"
+	"fairnest/internal/repository"
+	"fairnest/internal/service"
+	"github.com/minio/minio-go/v7/pkg/credentials"
+	"log"
+	"strings"
+	"time"
+
+	"github.com/spf13/viper"
+	"gorm.io/gorm"
+)
+
+const jwtSecret = "FriendSecret"
+
+func main() {
+	initTimeZone()
+	initConfig()
+	dsn := fmt.Sprintf("host=%v port=%v user=%v password=%v dbname=%v sslmode=disable TimeZone=Asia/Bangkok",
+		viper.GetString("db.host"),
+		viper.GetInt("db.port"),
+		viper.GetString("db.username"),
+		viper.GetString("db.password"),
+		viper.GetString("db.database"),
+	)
+	log.Println(dsn)
+
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+
+	if err != nil {
+		panic("Failed to connect database")
+	}
+
+	err = db.AutoMigrate(&entities.User{})
+	if err != nil {
+		panic("Failed to AutoMigrate User")
+	}
+
+	minioClient, err := minio.New(viper.GetString("minio.host")+":"+viper.GetString("minio.port"), &minio.Options{
+		Creds:  credentials.NewStaticV4("HDeAly8XddiQTvjjTRfL", "9OtiaCEOL1586uDgPAXNANDndM04ga3oMdGy7kGF", ""),
+		Secure: false,
+	})
+	if err != nil {
+		log.Fatalln(err)
+	}
+	fmt.Println("FairNest Minio connected")
+
+	uploadSer := service.NewUploadService(minioClient)
+	storageHandler := handler.NewStorageHandler(uploadSer)
+
+	userRepositoryDB := repository.NewUserRepositoryDB(db)
+
+	userService := service.NewUserService(userRepositoryDB, jwtSecret)
+	uploadService := service.NewUploadService(minioClient)
+
+	userHandler := handler.NewUserHandler(userService, jwtSecret, uploadService)
+
+	app := fiber.New()
+
+	app.Use(func(c *fiber.Ctx) error {
+		if c.Path() != "/Register" && c.Path() != "/Login" {
+			jwtMiddleware := jwtware.New(jwtware.Config{
+				SigningKey: jwtware.SigningKey{Key: []byte(jwtSecret)},
+				ErrorHandler: func(c *fiber.Ctx, err error) error {
+					return fiber.ErrUnauthorized
+				},
+			})
+			return jwtMiddleware(c)
+		}
+		return c.Next()
+	})
+
+	//Endpoint ###########################################################################
+
+	// Endpoint for test
+	app.Get("/GetUsers", userHandler.GetUsers)
+	app.Get("/GetUserByUserId/:UserID", userHandler.GetUserByUserId)
+	app.Get("/GetUserByToken", userHandler.GetUserByToken) //#
+
+	app.Post("/upload", storageHandler.UploadFile)
+
+	//////////////////////////////////////////////////////////////////////////////////////
+
+	// Endpoint for project
+	app.Post("/Register", userHandler.Register)
+	app.Post("/Login", userHandler.Login)
+
+	app.Get("/GetCurrentUser", userHandler.GetCurrentUser) //#
+	app.Get("/GetProfileOfCurrentUserByUserId/:UserID", userHandler.GetProfileOfCurrentUserByUserId)
+	app.Get("/GetEditUserProfileByUserId/:UserID", userHandler.GetEditUserProfileByUserId)
+	app.Patch("/PatchEditUserProfileByUserId/:UserID", userHandler.PatchEditUserProfileByUserId)
+
+	//#####################################################################################
+
+	log.Printf("NeedFul running at port:  %v", viper.GetInt("app.port"))
+	app.Listen(fmt.Sprintf(":%v", viper.GetInt("app.port")))
+
+}
+
+func initConfig() {
+	viper.SetConfigName("config")
+	viper.SetConfigType("yaml")
+	viper.AddConfigPath(".")
+	viper.AutomaticEnv()
+	viper.SetEnvKeyReplacer(strings.NewReplacer(".", "_"))
+
+	err := viper.ReadInConfig()
+	if err != nil {
+		panic(err)
+	}
+}
+
+func initTimeZone() {
+	ict, err := time.LoadLocation("Asia/Bangkok")
+	if err != nil {
+		panic(err)
+	}
+
+	time.Local = ict
+}
