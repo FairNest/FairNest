9f1f805c1f59980568666485d8da9d0e6e6b6e9b
Nithit Lertcharoensombat <xXNeonKitsuneXx@gmail.com>
2025-06-13 16:48:16 +0700

    Update fairnest_entities.go

--- server/internal/entities/fairnest_entities.go
+++ server/internal/entities/fairnest_entities.go
@@ -17,19 +17,44 @@ type User struct {
 }
 
 type LifestyleQuiz struct {
-	QuizID  *uint `gorm:"primaryKey;autoIncrement"`
-	UserID  *uint `gorm:"uniqueIndex"` // one-to-one
-	Answers *string
+	LifestyleQuizID       *uint `gorm:"primaryKey;autoIncrement"`
+	UserID                *uint `gorm:"uniqueIndex"` // one-to-one
+	Q1                    *int
+	Q2                    *int
+	Q3                    *int
+	Q4                    *int
+	Q5                    *int
+	Q6                    *int
+	Q7                    *int
+	Q8                    *int
+	Q9                    *int
+	Q10                   *int
+	Q11                   *int
+	Q12                   *int
+	UserOpenness          *float64
+	UserConscientiousness *float64
+	UserExtraversion      *float64
+	UserAgreeableness     *float64
+	UserNeuroticism       *float64
 
 	// Relations
 	User *User `gorm:"constraint:OnUpdate:CASCADE,OnDelete:CASCADE"`
 }
 
 type Room struct {
-	RoomID     *uint `gorm:"primaryKey;autoIncrement"`
-	LocationID *uint // one-to-one
-	Name       *string
-	Capacity   *uint
+	RoomID               *uint `gorm:"primaryKey;autoIncrement"`
+	LocationID           *uint // one-to-one
+	RoomName             *string
+	RoomDescription      *string
+	RoomType             *bool
+	RoomCode             *string
+	RoomCapacity         *int
+	OpennessAvg          *float64
+	ConscientiousnessAvg *float64
+	ExtraversionAvg      *float64
+	AgreeablenessAvg     *float64
+	NeuroticismAvg       *float64
+	CompatibilityScore   *float64
 
 	// Relations
 	Location    *Location
@@ -53,3 +78,23 @@ type RoomMember struct {
 	User *User `gorm:"constraint:OnUpdate:CASCADE,OnDelete:CASCADE"`
 	Room *Room `gorm:"constraint:OnUpdate:CASCADE,OnDelete:CASCADE"`
 }
+
+//type UserCompatibility struct {
+//	UserCompatibilityID *uint `gorm:"primaryKey;autoIncrement"`
+//
+//	RoomID  *uint `gorm:"not null"` // scope this match to a specific room
+//	UserAID *uint `gorm:"not null;index:idx_user_pair"`
+//	UserBID *uint `gorm:"not null;index:idx_user_pair"`
+//
+//	CompatibilityScore *float64
+//
+//	// Optional: prevent duplicate A-B and B-A pairs
+//	// Or enforce AID < BID ordering to avoid duplicates
+//
+//	CreatedAt time.Time
+//
+//	// Relations
+//	UserA *User `gorm:"foreignKey:UserAID;constraint:OnUpdate:CASCADE,OnDelete:CASCADE"`
+//	UserB *User `gorm:"foreignKey:UserBID;constraint:OnUpdate:CASCADE,OnDelete:CASCADE"`
+//	Room  *Room `gorm:"constraint:OnUpdate:CASCADE,OnDelete:CASCADE"`
+//}
